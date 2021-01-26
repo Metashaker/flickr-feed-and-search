afe8ee702bc570d39f0e09b13ad3ae7a7807811e
Metashaker <emilianopb92@gmail.com>
2021-01-26 16:23:37 -0600

    cleaned files + added data fetching logic to layout

--- frontend/src/layout/LayoutIndex.js
+++ frontend/src/layout/LayoutIndex.js
@@ -2,10 +2,21 @@ import React, { useContext, useEffect } from 'react';
 import HeaderIndex from '../components/Header/HeaderIndex';
 import { AppContext } from '../context/Context';
 
-const Layout = () => (
-  <>
-    <HeaderIndex />
-  </>
-);
+// eslint-disable-next-line object-curly-spacing
+// eslint-disable-next-line react/prop-types
+const Layout = ({ children }) => {
+  const { getInitialFeed } = useContext(AppContext);
+
+  useEffect(() => {
+    getInitialFeed();
+  }, []);
+
+  return (
+    <>
+      <HeaderIndex />
+      {children}
+    </>
+  );
+};
 
 export default Layout;
